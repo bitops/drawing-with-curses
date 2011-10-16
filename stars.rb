b7c85fdb2b476b2a463b9304206f443ab79aad8d
Sebastian Wittenkamp <a.sebastian.w@gmail.com>
2011-10-15 21:59:18 -0700

    forgot to add this class

---
+++ stars.rb
@@ -0,0 +1,17 @@
+require File.join('.', 'drawing')
+
+class Stars < Drawing
+
+	def	draw
+		write(rand(30), rand(30* 2), "*")
+		refresh
+		sleep 0.02
+	end
+	
+ 	def clear_count
+ 		300
+ 	end
+
+end
+
+Stars.new.run
